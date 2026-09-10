c19a9b1dde4cce3cd95074d6a40af43b1abc49ff
braiancalpar <braian.santos@calpar.com.br>
2026-09-10 15:22:20 -0300

    adiciona conexão e manipulação de eventos do socket.io no UserProvider

--- chat/src/hooks/user/provider.jsx
+++ chat/src/hooks/user/provider.jsx
@@ -1,27 +1,40 @@
-import { useEffect, useState } from 'react';
-import { userContext } from './context';
-import useChat from 'hooks/chat';
-import fetch from 'config/fetchInstance';
+import { useEffect, useState } from "react";
+import { userContext } from "./context";
+import useChat from "hooks/chat";
+import fetch from "config/fetchInstance";
+import { socket } from "config/socket";
 
 export const UserProvider = ({ children }) => {
   const [data, setData] = useState(null);
   const { data: chatData } = useChat();
 
   useEffect(() => {
-    const currentChatIndex = data?.chats.findIndex(chat => chat.id === chatData?.id);
+    socket.connect();
+    socket.on("nova mensagem", (id) => {
+      console.log("nova mensagem!", id);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const currentChatIndex = data?.chats.findIndex(
+      (chat) => chat.id === chatData?.id,
+    );
     if (data?.chats[currentChatIndex]?.unreadMessages > 0) {
-      setData(oldData => ({
+      setData((oldData) => ({
         ...oldData,
-        chats: oldData.chats.map(chat => {
+        chats: oldData.chats.map((chat) => {
           if (chat.id === chatData?.id) {
-            return { ...chat, unreadMessages: 0 }
+            return { ...chat, unreadMessages: 0 };
           }
           return chat;
-        })
-      }))
+        }),
+      }));
       fetch.post(`/api/chats/${chatData?.id}/readMessages`, { id: data.id });
     }
-  }, [data?.id, data?.chats, chatData?.id])
+  }, [data?.id, data?.chats, chatData?.id]);
 
   return (
     <userContext.Provider value={{ data, setData }}>
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,27 +1,28 @@
-import { io } from '../config/instances.js';
-import UserController from '../controllers/userController.js';
+import { io } from "../config/instances.js";
+import UserController from "../controllers/userController.js";
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   const userController = UserController();
+  io.emit("nova mensagem", socket.id);
 
-  socket.on('disconnect', () => {
-    io.emit('user disconnected');
+  socket.on("disconnect", () => {
+    io.emit("user disconnected");
     const id = null;
-    
-    userController.logoffUser(id, () => {
-      io.emit('user-logoff', id);
-    });
-  })
 
-  socket.on('logoff', (id) => {
     userController.logoffUser(id, () => {
-      io.emit('user-logoff', id);
+      io.emit("user-logoff", id);
     });
-  })
+  });
 
-  socket.on('add-user-id', (id) => {
+  socket.on("logoff", (id) => {
+    userController.logoffUser(id, () => {
+      io.emit("user-logoff", id);
+    });
+  });
+
+  socket.on("add-user-id", (id) => {
     userController.logonUser(id, socket, () => {
-      io.emit('user-logged', id);
+      io.emit("user-logged", id);
     });
-  })
-})
+  });
+});
